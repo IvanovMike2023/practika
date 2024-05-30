2c8c9e3948bd83a0c09e7e35170e855829030cee
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2024-05-30 14:37:05 +0300

    2

--- src/features/decks/DecksList/DeckItem/DeckItem.tsx
+++ src/features/decks/DecksList/DeckItem/DeckItem.tsx
@@ -2,6 +2,7 @@ import s from './DeckItem.module.css'
 import { useAppDispatch } from '../../../../app/store.ts'
 import { deleteDeckTC, updateDeckTC } from '../../decks-thunks.ts'
 import { Deck } from '../../decks-api.ts'
+import {useState} from "react";
 
 type DeckProps = {
   deck: Deck
@@ -12,14 +13,18 @@ const TEST_ACC_NAME = 'Sweety'
 export const DeckItem = ({ deck }: DeckProps) => {
   const isTestingDeck = deck.author.name === TEST_ACC_NAME
   const dispatch = useAppDispatch()
-
+const [disabled,Setdisabled]=useState(false)
   const handleDeleteButtonClick = () => {
-      console.log(dispatch(deleteDeckTC(deck.id)))
-    dispatch(deleteDeckTC(deck.id))
+      Setdisabled(true)
+    dispatch(deleteDeckTC(deck.id)).then((el)=>Setdisabled(false)
+    )
   }
 
   const handleEditButtonClick = () => {
-    dispatch(updateDeckTC({ id: deck.id, name: `${deck.name} updated` }))
+      Setdisabled(true)
+      dispatch(updateDeckTC({ id: deck.id, name: `${deck.name} updated` })).then(()=>{
+          Setdisabled(false)
+      })
   }
 
   return (
@@ -40,8 +45,8 @@ export const DeckItem = ({ deck }: DeckProps) => {
 
       {isTestingDeck && (
         <div className={s.buttonBox}>
-          <button onClick={handleEditButtonClick}>update</button>
-          <button onClick={handleDeleteButtonClick}>delete</button>
+          <button onClick={handleEditButtonClick} disabled={disabled}>update</button>
+          <button onClick={handleDeleteButtonClick} disabled={disabled}>delete</button>
         </div>
       )}
     </li>
