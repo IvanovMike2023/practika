6129251e9a5d07efa6c8c35a358b217cd5f9938b
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2024-05-31 14:17:13 +0300

    3

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -1,32 +1,42 @@
-import { Dispatch } from 'redux'
-import { decksAPI, UpdateDeckParams } from './decks-api.ts'
-import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
+import {Dispatch} from 'redux'
+import {decksAPI, UpdateDeckParams} from './decks-api.ts'
+import {addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC} from './decks-reducer.ts'
 import {createLogger} from "vite";
 import {AppSetatusAC} from "../../app/app-reducer";
 
 export const fetchDecksTC = () => (dispatch: Dispatch) => {
-  dispatch(AppSetatusAC('loading'))
-  decksAPI.fetchDecks().then((res) => {
-    dispatch(setDecksAC(res.data.items))
-    dispatch(AppSetatusAC('succeeded'))
+    dispatch(AppSetatusAC('loading'))
+    decksAPI.fetchDecks().then((res) => {
+        dispatch(setDecksAC(res.data.items))
+        dispatch(AppSetatusAC('succeeded'))
 
-  }).catch()
+    }).catch()
 }
 
 export const addDeckTC = (name: string) => async (dispatch: Dispatch) => {
-  return decksAPI.addDeck(name).then((res) => {
-    dispatch(addDeckAC(res.data))
-  })
+    return decksAPI.addDeck(name).then((res) => {
+        dispatch(addDeckAC(res.data))
+    })
 }
 
 export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
-  return decksAPI.deleteDeck(id).then((res) => {
-    dispatch(deleteDeckAC(res.data.id))
-  })
+    return decksAPI.deleteDeck(id).then((res) => {
+        dispatch(deleteDeckAC(res.data.id))
+    })
 }
 
 export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
-  return decksAPI.updateDeck(params).then((res) => {
-    dispatch(updateDeckAC(res.data))
-  })
+    try {
+        // throw new Error('boom!')
+        const res = await decksAPI.updateDeck(params).then((res) => {
+            dispatch(updateDeckAC(res.data))
+        })
+    } catch (e) {
+        if (e.response) {
+            console.log(e.response.data.errorMessages[0].message)
+        } else if (e.message) {
+            console.log(e.message)
+        } else console.log({e: e.message.e})
+    }
 }
+
